b8dfebec76a33439a6cf1ea8f5ab22e08214357c
henrijup <henrijup@metropolia.fi>
2016-05-03 12:17:41 +0300

    Update Main.java
    Tein random olion joka tuodaan RemoteControlliin, jota käytetään autopilotissa

--- Main.java
+++ Main.java
@@ -1,14 +1,16 @@
 package robotProject;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3IRSensor;
+import java.util.Random;
 
 public class Main {
 
 	public static void main(String[] args) {
 		EV3IRSensor irsSensor = new EV3IRSensor(SensorPort.S4);
+		Random randomGenerator = new Random();
 		Motors motors = new Motors();
 		IRSChecker irs = new IRSChecker(irsSensor);
-		RemoteControl rmt = new RemoteControl(irs,motors);
+		RemoteControl rmt = new RemoteControl(irs,motors,randomGenerator);
 		rmt.run();
 	}
 
